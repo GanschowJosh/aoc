e6bbb8751d21e9c325454e00f0708930904d6a7e
GanschowJosh <ganschowjosh@gmail.com>
2026-06-30 15:26:53 -0500

    2023/9

---
+++ 2023/9/b.py
@@ -0,0 +1,28 @@
+with open(input(), 'r') as f:
+  lines=f.readlines()
+
+hist=[]
+for line in lines:
+  hist.append(list(map(int, line.strip().split())))
+
+def next_seq(seq):
+  if len(seq) == 1: return [0]
+  out=[]
+  for i, s in enumerate(seq):
+    if i == 0: continue
+    out.append(s-seq[i-1])
+  return out
+
+su=0
+for h in hist:
+  curr = h
+  done=[]
+  while not all(c==0 for c in curr):
+    done.append(curr)
+    curr=next_seq(curr)
+  carry=0
+  for d in reversed(done):
+    carry=d[0]-carry
+  su+=carry
+
+print(su)
